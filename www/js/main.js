0330455fdb307fea3362c1e5b85c72f2d36f9c17
Seth Whitaker <sdw3489@gmail.com>
2016-10-14 10:05:27 -0600

    style fixes for dashboard

--- www/js/main.js
+++ www/js/main.js
@@ -1,12 +1,24 @@
 (function($) {
 	'use strict';
-	new WOW({
-		offset: 50,
-		mobile: true
-	}).init();
 
 	$(document).ready(function(){
 
+		function scrollTo(e){
+			e.preventDefault;
+
+		    var $this = $(this),
+		        $target = $(this.hash);
+
+		    if( $target.length ){
+		      $('html,body').animate({
+		          scrollTop: $target.offset().top
+		        }, 700, 'swing');
+		    }
+
+		    return false;
+		}
+
+		$('[data-scroll-to]').on('click', scrollTo);
 
 	});
 })(jQuery);
